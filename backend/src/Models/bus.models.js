4d6bc5d3122d75fc656c1b08ea84f01d4eff240e
hosurVijay <hosurvijayendra@gmail.com>
2025-09-17 01:27:21 +0530

    All the Scheam added (bus, route, tracking) models

--- backend/src/Models/bus.models.js
+++ backend/src/Models/bus.models.js
@@ -1,3 +1,36 @@
 import mongoose, { Schema } from "mongoose";
 
-const busSchema = new Schema({});
+const busSchema = new Schema({
+  busRouteId: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+  },
+  timing: {
+    type: Date,
+    required: true,
+  },
+  busNumber: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true,
+    lowercase: true,
+  },
+  status: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+  },
+  lastUpdated: {
+    type: Date,
+    required: true,
+    default: Date.now,
+  },
+});
+
+const Bus = mongoose.model("Bus", busSchema);
+
+export { Bus };
---
+++ backend/src/Models/busRoutes.models.js
@@ -0,0 +1,46 @@
+import mongoose, { Schema, Types } from "mongoose";
+
+const busRouteSchema = new Schema(
+  {
+    routeName: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+    buses: [
+      {
+        type: Types.ObjectId,
+        ref: "Bus",
+        required: true,
+      },
+    ],
+    stops: [
+      {
+        type: String,
+        lowercase: true,
+        required: true,
+      },
+    ],
+    totalDistance: {
+      type: Number,
+      required: false,
+      default: null,
+    },
+    currentStop: {
+      type: String,
+      required: false,
+      default: null,
+    },
+    nextStop: {
+      type: String,
+      required: false,
+      default: null,
+    },
+  },
+  { timestamps: true }
+);
+
+const BusRoute = mongoose.model("BusRoute", busRouteSchema);
+
+export { BusRoute };
