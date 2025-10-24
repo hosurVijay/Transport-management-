74039c436b4b968c213d09f992b2dc144d693c0a
hosurVijay <hosurvijayendra@gmail.com>
2025-10-24 20:06:21 +0530

    all the api endpoint v1 added

--- backend/src/app.js
+++ backend/src/app.js
@@ -12,7 +12,16 @@ app.use(express.urlencoded({ limit: "16kb" }));
 app.use(express.static("public"));
 app.use(express.json({ limit: "16kb" }));
 
-import busSignalingRoute from "./Routes/busSignalling.routes.js";
+import busSignalingRoute from "./Routes.esp/busSignalling.routes.js";
 
 app.use("/api/v1/bus", busSignalingRoute);
 export { app };
+
+import manualSignallingRoute from "./Routes.web/manualBusSignalling.routes.js";
+app.use("/api/v1/busManual", manualSignallingRoute);
+
+import getBusProgress from "./Routes.web/routes.route.js";
+app.use("/api/v1/busProgress", getBusProgress);
+
+import getBusLocation from "./Routes.web/trackingBus.route.js";
+app.use("/api/v1/busLocation", getBusLocation);
